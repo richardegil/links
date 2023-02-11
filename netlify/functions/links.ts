af1267e20b499038e8573038aba55194ab26312e
Richard Gil <richard@richardegil.com>
2023-02-11 10:37:18 -0600

    adding netlify function and tailwind

---
+++ netlify/functions/links.ts
@@ -0,0 +1,94 @@
+import fetch from 'node-fetch';
+import { Handler, HandlerEvent, HandlerContext, builder } from "@netlify/functions";
+
+export type LinkItem = {
+  id: string;
+  name: string;
+  url: string;
+};
+
+function parseNotionDatabase(data): LinkItem[] {
+  const results = data.results;
+
+  return results?.map((result): LinkItem => {
+      const { name, url } =
+        result.properties;
+
+      return {
+        id: result.id,
+        name: name.title[0].plain_text,
+        url: url.rich_text[0].href
+      };
+    })
+}
+
+export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+  const notionSecret = process.env.NOTION_SECRET;
+  const notionDatabaseID = process.env.NOTION_DB_ID;
+
+  const response = await fetch(
+    `https://api.notion.com/v1/databases/${notionDatabaseID}/query/`,
+    {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${notionSecret}`,
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-06-28',
+      },
+    },
+  );
+
+  if (!response.ok) {
+    console.error(response);
+  
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'unable to load data from Notion' }),
+    };
+  }
+  
+  const data = await response.json();
+  const items = parseNotionDatabase(data);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(items),
+  };  
+}
+
+
+// const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
+//   const notionSecret = process.env.NOTION_SECRET;
+//   const notionDatabaseID = process.env.NOTION_DB_ID;
+
+//   const response = await fetch(
+//     `https://api.notion.com/v1/databases/${notionDatabaseID}`,
+//     {
+//       method: 'GET',
+//       headers: {
+//         Authorization: `Bearer ${notionSecret}`,
+//         'Content-Type': 'application/json',
+//         'Notion-Version': '2022-06-28',
+//       },
+//     },
+//   );
+
+//   if (!response.ok) {
+//     console.error(response);
+  
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'unable to load data from Notion' }),
+//     };
+//   }
+  
+//   const data = await response.json();
+//   const items = parseNotionDatabase(data);
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(items),
+//   };  
+// };
+
+// export { handler };
