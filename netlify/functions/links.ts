0ecc78152e57412252a75271d0ce3f82338e277a
Richard Gil <richard@richardegil.com>
2023-02-11 18:44:27 -0600

    fixing the order of links

--- netlify/functions/links.ts
+++ netlify/functions/links.ts
@@ -3,23 +3,27 @@ import { Handler, HandlerEvent, HandlerContext, builder } from "@netlify/functio
 
 export type LinkItem = {
   id: string;
+  order: string;
   name: string;
   url: string;
 };
 
 function parseNotionDatabase(data): LinkItem[] {
   const results = data.results;
+  
 
   return results?.map((result): LinkItem => {
-      const { name, url } =
+      const { name, url, number } =
         result.properties;
 
       return {
         id: result.id,
+        order: number.rich_text[0].plain_text,
         name: name.title[0].plain_text,
         url: url.rich_text[0].href
       };
     })
+    .sort((a, b) => a.order - b.order)
 }
 
 export const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
@@ -55,40 +59,3 @@ export const handler: Handler = async (event: HandlerEvent, context: HandlerCont
     body: JSON.stringify(items),
   };  
 }
-
-
-// const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-//   const notionSecret = process.env.NOTION_SECRET;
-//   const notionDatabaseID = process.env.NOTION_DB_ID;
-
-//   const response = await fetch(
-//     `https://api.notion.com/v1/databases/${notionDatabaseID}`,
-//     {
-//       method: 'GET',
-//       headers: {
-//         Authorization: `Bearer ${notionSecret}`,
-//         'Content-Type': 'application/json',
-//         'Notion-Version': '2022-06-28',
-//       },
-//     },
-//   );
-
-//   if (!response.ok) {
-//     console.error(response);
-  
-//     return {
-//       statusCode: 500,
-//       body: JSON.stringify({ error: 'unable to load data from Notion' }),
-//     };
-//   }
-  
-//   const data = await response.json();
-//   const items = parseNotionDatabase(data);
-
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify(items),
-//   };  
-// };
-
-// export { handler };
